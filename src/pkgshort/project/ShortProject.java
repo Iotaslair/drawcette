9ef9a3cbe2f8d436e8d07b5e2885f5e69efee7ea
J_Johnson <jeremy.johnson14@ncf.edu>
2018-02-07 10:27:51 -0500

    Created the ShortProject.java class. It currently creates a window as a canvas and allows the user to draw lines, albeit all through the middle. The line drawing code should be moved elsewhere eventually.

---
+++ src/pkgshort/project/ShortProject.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javafx.scene.shape.Line;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ * @author jerem
+ */
+public class ShortProject extends Application {
+    
+    Boolean firstClick = true;
+    double storedx = -1;
+    double storedy = -1;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        //Button btn = new Button();
+        //btn.setText("Say 'Hello World'");
+        //btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+        //    @Override
+        //    public void handle(ActionEvent event) {
+        //        System.out.println("Hello World!");
+        //    }
+        //});
+        
+        StackPane root = new StackPane();
+        //root.getChildren().add(btn);
+
+        
+        Scene scene = new Scene(root, 500, 500);
+        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println(event.getX() + ", " + event.getY());
+                if (firstClick){
+                    firstClick = false;
+                    storedx = event.getX();
+                    storedy = event.getY();
+                }
+                else {
+                    firstClick = true;
+                    // Make line
+                    Line line = new Line();
+                    line.setStartX(storedx);
+                    line.setStartY(storedy);
+                    line.setEndX(event.getX());
+                    line.setEndY(event.getY());
+                    
+                    root.getChildren().add(line);
+                    
+                    storedx = -1;
+                    storedy = -1;
+                }
+            }
+        });
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+        
+}
