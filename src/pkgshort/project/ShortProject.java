7211f43a4eeeb6b9d8ac583d5d9f6f0bc6c5a444
Andrew Leonov <mr.andreleon@gmail.com>
2018-02-07 18:14:23 -0500

    Added menus

--- src/pkgshort/project/ShortProject.java
+++ src/pkgshort/project/ShortProject.java
@@ -1,14 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-
-import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.application.Application;
 import javafx.stage.Stage;
-
-import javafx.scene.shape.Line;
-import javafx.scene.layout.Pane;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.*;
+import javafx.scene.input.*;
 
 /**
  *
@@ -32,11 +38,18 @@ public class ShortProject extends Application {
         //    }
         //});
         
-        Pane root = new Pane();
+        BorderPane root = new BorderPane();
         //root.getChildren().add(btn);
 
+        MenuBar menuBar = createMenu();
+        VBox menuBox = addVBox();
         
-        Scene scene = new Scene(root, 500, 500);
+        // Set both panes' positions on main pane
+        root.setTop(menuBar);
+        root.setLeft(menuBox);
+        
+        Scene scene = new Scene(root, 800, 800);
+        
         scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -63,7 +76,7 @@ public class ShortProject extends Application {
             }
         });
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Photopoop");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -73,6 +86,58 @@ public class ShortProject extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public MenuBar createMenu() {
+        // Create MenuBar
+        MenuBar menuBar = new MenuBar();
+        
+        // Create menus
+        Menu fileMenu = new Menu("File");
+        Menu exportMenu = new Menu("Export");
+        
+        // Create Menu Items
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openFileItem = new MenuItem("Open File");
+        MenuItem exitItem = new MenuItem("Exit");
+        
+        MenuItem exportSvgItem = new MenuItem("Export into SVG");
+        MenuItem printItem = new MenuItem("Print");
+        
+        // Add menuItems to the Menus
+        fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
+        exportMenu.getItems().addAll(exportSvgItem, printItem);
+        
+        // Add Menus to the MenuBar
+        menuBar.getMenus().addAll(fileMenu, exportMenu);
+        
+        return menuBar;
+    }
+    
+    public VBox addVBox() {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(15, 12, 15, 12));
+        vbox.setSpacing(10);
+        vbox.setStyle("-fx-background-color: #336699;");
+
+        Button btn1 = new Button("1");
+        btn1.setPrefSize(20, 20);
+
+        Button btn2 = new Button("2");
+        btn2.setPrefSize(20, 20);
+
+        Button btn3 = new Button("3");
+        btn3.setPrefSize(20, 20);
+
+        Button btn4 = new Button("4");
+        btn4.setPrefSize(20, 20);
+
+        Button btn5 = new Button("5");
+        btn5.setPrefSize(20, 20);
+
+        vbox.getChildren().addAll(btn1, btn2, btn3, btn4, btn5);
+
+        return vbox;
     }
         
 }
