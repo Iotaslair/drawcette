2f62647ce56e2b13dd24861b262a7d40c0a8ba60
Andrew Leonov <mr.andreleon@gmail.com>
2018-02-08 23:55:39 -0500

    Added Color Picker

--- src/pkgshort/project/ShortProject.java
+++ src/pkgshort/project/ShortProject.java
@@ -32,14 +32,16 @@ public class ShortProject extends Application {
 
         MenuBar menuBar = createMenu();
         VBox menuBox = createSidebar();
+        HBox colorPicker = createBottomToolbar();
         Pane surface = addSurface();
         
         // Set both panes' positions on main pane
         root.setTop(menuBar);
         root.setLeft(menuBox);
+        root.setBottom(colorPicker);
         root.setCenter(surface);
         
-        Scene scene = new Scene(root, 800, 800);
+        Scene scene = new Scene(root, 1000, 800);
                 
         primaryStage.setTitle("Photopoop");
         primaryStage.setScene(scene);
@@ -132,6 +134,71 @@ public class ShortProject extends Application {
         return vbox;
     }
     
+    public HBox createBottomToolbar() {
+    	HBox hbox = new HBox();
+    	
+    	// Set up the panel
+        hbox.setPadding(new Insets(20, 20, 15, 15));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #444;");
+        
+        // Color Palette
+        final ToggleGroup group = new ToggleGroup();
+        
+        ToggleButton color1 = new ToggleButton();
+        color1.setToggleGroup(group);
+        // color1.setUserData(Color.RED);
+        color1.setSelected(true);
+        color1.setPrefSize(30, 30);
+        color1.setStyle("-fx-base: red;");
+
+        ToggleButton color2 = new ToggleButton();
+        color2.setToggleGroup(group);
+        color2.setUserData(Color.ORANGE);
+        color2.setPrefSize(30, 30);
+        color2.setStyle("-fx-base: orange;");
+
+        ToggleButton color3 = new ToggleButton();
+        color3.setToggleGroup(group);
+        color3.setUserData(Color.YELLOW);
+        color3.setPrefSize(30, 30);
+        color3.setStyle("-fx-base: yellow;");
+        
+        ToggleButton color4 = new ToggleButton();
+        color4.setToggleGroup(group);
+        color4.setUserData(Color.GREEN);
+        color4.setPrefSize(30, 30);
+        color4.setStyle("-fx-base: green;");
+
+        ToggleButton color5 = new ToggleButton();
+        color5.setToggleGroup(group);
+        color5.setUserData(Color.BLUE);
+        color5.setPrefSize(30, 30);
+        color5.setStyle("-fx-base: blue;");
+
+        ToggleButton color6 = new ToggleButton();
+        color6.setToggleGroup(group);
+        color6.setUserData(Color.INDIGO);
+        color6.setPrefSize(30, 30);
+        color6.setStyle("-fx-base: indigo;");
+
+        ToggleButton color7 = new ToggleButton();
+        color7.setToggleGroup(group);
+        color7.setUserData(Color.VIOLET);
+        color7.setPrefSize(30, 30);
+        color7.setStyle("-fx-base: violet;");
+
+        ToggleButton color8 = new ToggleButton();
+        color8.setToggleGroup(group);
+        color8.setUserData(Color.BLACK);
+        color8.setPrefSize(30, 30);
+        color8.setStyle("-fx-base: black;");
+
+        hbox.getChildren().addAll(color1, color2, color3, color4, color5, color6, color7, color8);
+    	
+    	return hbox;
+    }
+    
     public Pane addSurface() {
         Pane surface = new Pane();
         
@@ -145,9 +212,9 @@ public class ShortProject extends Application {
                 drawLine(surface, event);       
             }
         });
-        
+
         return surface;
-    } 
+    }
 
     public static void onAction(Menu menu) {
         final MenuItem menuItem = new MenuItem();
@@ -171,11 +238,12 @@ public class ShortProject extends Application {
             firstClick = true;
             // Make line
             Line line = new Line();
+            
             line.setStartX(storedx);
             line.setStartY(storedy);
             line.setEndX(event.getX());
             line.setEndY(event.getY());
-                    
+            
             surface.getChildren().add(line);
                     
             storedx = -1;
